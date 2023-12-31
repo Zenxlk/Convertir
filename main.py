77a007b51ffc07b21cdfd79bd14b2250c5d19e43
Zenxlk <jorgejair5678@gmail.com>
2023-12-30 19:49:12 -0500

    Funcionalidad opciones V1

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Convertidor de diferentes unidades
-
-# Version 1.0
 # Autor: Zenxlk
 
 # Definir menú
@@ -14,15 +11,6 @@
 # Importar librerias para interfaz grafica
 
 from tkinter import *
-from tkinter import messagebox
-
-
-# Informacion
-
-from tkinter import *
-from tkinter import messagebox
-from tkinter import *
-from tkinter import messagebox
 
 # Intro
 
@@ -127,7 +115,7 @@ def convertir_longitud():
 
     # Calcular la posición de la ventana para que se muestre en el centro de la pantalla
     ventana_ancho = 300
-    ventana_alto = 200
+    ventana_alto = 210
     pantalla_ancho = ventana.winfo_screenwidth()
     pantalla_alto = ventana.winfo_screenheight()
     posicion_x = (pantalla_ancho - ventana_ancho) // 2
@@ -150,28 +138,84 @@ def convertir_longitud():
     lista_desplegable1 = OptionMenu(ventana, seleccion1, *unidades_longitud)
     lista_desplegable1.pack()
 
+    # Seperador con label
+    label = Label(
+        ventana,
+        text="a:",
+        fg="black",
+        font=("Helvetica", 10),
+        anchor="center",
+        justify="center",
+    )
+    label.pack()
+
     lista_desplegable2 = OptionMenu(ventana, seleccion2, *unidades_longitud)
     lista_desplegable2.pack()
 
+    # Separador label
+    label2 = Label(
+        ventana,
+        text=" "
+    )
+
+    label2.pack()
+    label = Label(
+        ventana,
+        text="Ingrese cantidad a converetir:",
+        fg="black",
+        font=("Helvetica", 10),
+        anchor="center",
+        justify="center",
+    )
+    label.pack()
+
     # Input field
-    input_valor = Entry(ventana)
+    input_valor = Entry(
+        ventana,
+        fg="black",
+        borderwidth=2,
+        relief="solid"
+    )
     input_valor.pack()
+
+    # Variable resultado
+    resultado = 0
 
     # Conversion button
     def calcular_conversion():
         valor = float(input_valor.get())
         unidad_origen = seleccion1.get()
         unidad_destino = seleccion2.get()
-        
-        #Metro
-        if unidad_origen == "Metro" and unidad_destino == "Metro":
-            unidad_destino = valor
-            unidad_destino = valor
-            
-            
+
+        conversion_rates = {
+            "Metro": 1,
+            "Kilómetro": 1000,
+            "Pulgada": 0.0254,
+            "Pie": 0.3048,
+            "Yarda": 0.9144,
+            "Milla": 1609.34
+        }
+
+        if unidad_origen in conversion_rates and unidad_destino in conversion_rates:
+            resultado = valor * \
+                conversion_rates[unidad_origen] / \
+                conversion_rates[unidad_destino]
+            resultado_label.config(text=f"La conversión es: {
+                                   resultado} {unidad_destino}s")
+        else:
+            resultado = 0
+            print(f"Unidad de origen: {unidad_origen}, Unidad de destino: {
+                  unidad_destino} el resultado es: {resultado} ")
+
+    # Label de resultado
+    resultado_label = Label(ventana, text="Ingrese datos...")
+    resultado_label.pack()
+
     boton_calcular = Button(ventana, text="Calcular",
                             command=calcular_conversion)
     boton_calcular.pack()
+
+    boton_calcular.config(command=calcular_conversion)
 
     ventana.mainloop()
 
@@ -207,11 +251,86 @@ def convertir_masa():
 
     # Unidades de masa
     unidades_masa = ["Gramo", "Kilogramo", "Onza", "Libra", "Tonelada"]
-    seleccion = StringVar(ventana)
-    seleccion.set(unidades_masa[0])  # Posicion
+    seleccion1 = StringVar(ventana)
+    seleccion1.set(unidades_masa[0])  # Posicion
 
-    lista_desplegable = OptionMenu(ventana, seleccion, *unidades_masa)
-    lista_desplegable.pack()
+    seleccion2 = StringVar(ventana)
+    seleccion2.set(unidades_masa[0])  # Posicion
+
+    lista_desplegable1 = OptionMenu(ventana, seleccion1, *unidades_masa)
+    lista_desplegable1.pack()
+
+    # Seperador con label
+    label = Label(
+        ventana,
+        text="a:",
+        fg="black",
+        font=("Helvetica", 10),
+        anchor="center",
+        justify="center",
+    )
+    label.pack()
+
+    lista_desplegable2 = OptionMenu(ventana, seleccion2, *unidades_masa)
+    lista_desplegable2.pack()
+
+    # Separador label
+    label3 = Label(
+        ventana,
+        text=" "
+    )
+    label3.pack()
+
+    # Separador label
+    label2 = Label(
+        ventana,
+        text="Ingrese cantidad a convertir:"
+    )
+    label2.pack()
+
+    input_valor = Entry(
+        ventana,
+        fg="black",
+        borderwidth=2,
+        relief="solid"
+    )
+    input_valor.pack()
+
+    # Boton conversion
+    def calcular_conversion():
+        valor = float(input_valor.get())
+        unidad_origen = seleccion1.get()
+        unidad_destino = seleccion2.get()
+
+        conversion_rates = {
+            "Gramo": 1,
+            "Kilogramo": 1000,
+            "Onza": 28.3495,
+            "Libra": 453.592,
+            "Tonelada": 1000000
+        }
+
+        if unidad_origen in conversion_rates and unidad_destino in conversion_rates:
+            resultado = valor * \
+                conversion_rates[unidad_origen] / \
+                conversion_rates[unidad_destino]
+        else:
+            resultado = 0
+            print(f"Unidad de origen: {unidad_origen}, Unidad de destino: {
+                  unidad_destino} el resultado es: {resultado}")
+
+        resultado_label.config(text=f"La conversión es: {
+                               resultado} {unidad_destino}s")
+
+    # Label de resultado
+    resultado_label = Label(ventana, text="Ingrese datos...")
+    resultado_label.pack()
+
+    boton_calcular = Button(ventana, text="Calcular",
+                            command=calcular_conversion)
+    boton_calcular.pack()
+
+    boton_calcular.config(command=calcular_conversion)
 
     ventana.mainloop()
 
@@ -245,14 +364,86 @@ def convertir_temperatura():
     ventana.geometry(f"{ventana_ancho}x{
                      ventana_alto}+{posicion_x}+{posicion_y}")
 
-    # Unidades de temperatura
+    # Unidades de temperatura}
+    unidades_temperatura = ["Celsius", "Fahrenheit", "Kelvin"]
+    seleccion1 = StringVar(ventana)
+    seleccion1.set(unidades_temperatura[0])  # Posicion
 
-    unidades_temperatura = ["Celcius", "Farenheit"]
-    seleccion = StringVar(ventana)
-    seleccion.set(unidades_temperatura[0])  # Posicion
+    seleccion2 = StringVar(ventana)
+    seleccion2.set(unidades_temperatura[0])  # Posicion
 
-    lista_desplegable = OptionMenu(ventana, seleccion, *unidades_temperatura)
-    lista_desplegable.pack()
+    lista_desplegable1 = OptionMenu(ventana, seleccion, *unidades_temperatura)
+    lista_desplegable1.pack()
+
+    # Seperador con label
+    label = Label(
+        ventana,
+        text="a:",
+        fg="black",
+        font=("Helvetica", 10),
+        anchor="center",
+        justify="center",
+    )
+    label.pack()
+
+    lista_desplegable2 = OptionMenu(ventana, seleccion, *unidades_temperatura)
+    lista_desplegable2.pack()
+
+    # Separador label
+    label2 = Label(
+        ventana,
+        text=" "
+    )
+    label2.pack()
+
+    # Separador label
+    label2 = Label(
+        ventana,
+        text="Ingrese cantidad a convertir:"
+    )
+    label2.pack()
+
+    input_valor = Entry(
+        ventana,
+        fg="black",
+        borderwidth=2,
+        relief="solid"
+    )
+    input_valor.pack()
+
+    # Boton conversion
+    def calcular_conversion():
+        valor = float(input_valor.get())
+        unidad_origen = seleccion1.get()
+        unidad_destino = seleccion2.get()
+
+        conversion_rates = {
+            "Celsius": 1,
+            "Fahrenheit": 1.8,
+            "Kelvin": 1
+        }
+
+        if unidad_origen in conversion_rates and unidad_destino in conversion_rates:
+            resultado = valor * \
+                conversion_rates[unidad_origen] / \
+                conversion_rates[unidad_destino]
+        else:
+            resultado = 0
+            print(f"Unidad de origen: {unidad_origen}, Unidad de destino: {
+                  unidad_destino} el resultado es: {resultado}")
+
+        resultado_label.config(text=f"La conversión es: {
+                               resultado} {unidad_destino}s")
+
+    # Label de resultado
+    resultado_label = Label(ventana, text="Ingrese datos...")
+    resultado_label.pack()
+
+    boton_calcular = Button(ventana, text="Calcular",
+                            command=calcular_conversion)
+    boton_calcular.pack()
+
+    boton_calcular.config(command=calcular_conversion)
 
     ventana.mainloop()
 
@@ -287,12 +478,90 @@ def convertir_tiempo():
                      ventana_alto}+{posicion_x}+{posicion_y}")
 
     # Unidades de tiempo
-    unidades_tiempo = ["Años", "Meses", "Dias"]
-    seleccion = StringVar(ventana)
-    seleccion.set(unidades_tiempo[0])  # Posicion
+    unidades_tiempo = ["Segundo", "Minuto",
+                       "Hora", "Día", "Semana", "Mes", "Año"]
+    seleccion1 = StringVar(ventana)
+    seleccion1.set(unidades_tiempo[0])  # Posicion
 
-    lista_desplegable = OptionMenu(ventana, seleccion, *unidades_tiempo)
-    lista_desplegable.pack()
+    seleccion2 = StringVar(ventana)
+    seleccion2.set(unidades_tiempo[0])  # Posicion
+
+    lista_desplegable1 = OptionMenu(ventana, seleccion1, *unidades_tiempo)
+    lista_desplegable1.pack()
+
+    # Seperador con label
+    label = Label(
+        ventana,
+        text="a:",
+        fg="black",
+        font=("Helvetica", 10),
+        anchor="center",
+        justify="center",
+    )
+    label.pack()
+
+    lista_desplegable2 = OptionMenu(ventana, seleccion2, *unidades_tiempo)
+    lista_desplegable2.pack()
+
+    # Separador label
+    label2 = Label(
+        ventana,
+        text=" "
+    )
+    label2.pack()
+
+    # Separador label
+    label2 = Label(
+        ventana,
+        text="Ingrese cantidad a convertir:"
+    )
+    label2.pack()
+
+    input_valor = Entry(
+        ventana,
+        fg="black",
+        borderwidth=2,
+        relief="solid"
+    )
+    input_valor.pack()
+
+    # Boton conversion
+    def calcular_conversion():
+        valor = float(input_valor.get())
+        unidad_origen = seleccion1.get()
+        unidad_destino = seleccion2.get()
+
+        conversion_rates = {
+            "Segundo": 1,
+            "Minuto": 60,
+            "Hora": 3600,
+            "Día": 86400,
+            "Semana": 604800,
+            "Mes": 2629800,
+            "Año": 31557600
+        }
+
+        if unidad_origen in conversion_rates and unidad_destino in conversion_rates:
+            resultado = valor * \
+                conversion_rates[unidad_origen] / \
+                conversion_rates[unidad_destino]
+        else:
+            resultado = 0
+            print(f"Unidad de origen: {unidad_origen}, Unidad de destino: {
+                  unidad_destino} el resultado es: {resultado}")
+
+        resultado_label.config(text=f"La conversión es: {
+                               resultado} {unidad_destino}s")
+
+    # Label de resultado
+    resultado_label = Label(ventana, text="Ingrese datos...")
+    resultado_label.pack()
+
+    boton_calcular = Button(ventana, text="Calcular",
+                            command=calcular_conversion)
+    boton_calcular.pack()
+
+    boton_calcular.config(command=calcular_conversion)
 
     ventana.mainloop()
 
